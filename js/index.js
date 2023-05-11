5112fbf4ebdb6c7c09c52a7185a38a8278f3939a
KaylaPosthumusOW <231096@virtualwindow.co.za>
2023-05-11 10:14:15 +0200

    Data types

---
+++ js/index.js
@@ -0,0 +1,48 @@
+1.
+var firstName = "Kayla";
+var surname = "Muller";
+var integer = "19";
+const subject = "Development";
+
+let message = "Hello, my name is " + firstName + " " + surname + ", I am " + integer + " years old and I am studying " + subject + "."
+
+console.log(message)
+
+2.
+let numOne = 3
+let numTwo = 5
+let numThree = 5
+let numFour = 0
+let answer = 0
+
+let formula = [numOne + numTwo] / 5 * 0
+
+console.log(formula)
+
+
+3.
+let timmy = (15/100) * 10;
+let susan = (26/100) * 25;
+
+let value = timmy + susan
+
+let output = "The tax man will claim R" + value + " from Timmy and Susan."
+
+console.log(output)
+
+4.
+let john = 80 
+let tim = 90
+let alfie = 75
+let gwen = 60
+
+john = john + 7
+tim = tim + 7
+alfie = alfie + 7
+gwen = gwen + 7
+
+let equals = john + tim + alfie + gwen
+let average = (equals/400) * 100
+
+console.log(average)
+
